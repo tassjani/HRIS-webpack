3a46705eb08e80bb7fa7f4efd5b1250da8deb4f4
Your Name <you@example.com>
2020-09-25 14:54:42 +0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'none',
+  entry: {
+    app: path.join(__dirname, 'src', 'index.tsx')
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: { transpileOnly: true },
+      },
+      {
+        test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader', options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('src/index.html')
+    })
+  ]
+}
